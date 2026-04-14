e19ef2039a232e8495f0363433fdb67e7c218217
unknown <ocguerreros@unal.edu.co>
2026-04-14 12:12:18 -0500

    primer prueba de codigo

--- models/user.js
+++ models/user.js
@@ -1,19 +1,12 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    
-    phone: {
-        type: String,
-        unique: true
-    },
-    
+    phone: { type: String, unique: true },
     profile: String,
     lastIntent: String,
-    messages: Number ,
-    
-    createdAt: {
-        type: Date,
-        degault: Date.now
-    }
-
+    step: { type: String, default: null },
+    messages: { type: Number, default: 0 },
+    createdAt: { type: Date, default: Date.now }
 });
+
+module.exports = mongoose.model("User", userSchema);
